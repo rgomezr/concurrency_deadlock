b8d599edda9b06cc9fe4573b4cd67f6c825c7b38
rgomezr <ramon@ramongomez.xyz>
2022-09-27 09:31:15 +0200

    Created async op for validating uk postcode

---
+++ concurrency_deadlock/Program.cs
@@ -0,0 +1,24 @@
+﻿const string GET_BASE_URL = "https://api.postcodes.io/postcodes/";
+
+Dictionary<bool, string> responseDict = await IsUKPostcodeValid("BT35 8HR");
+
+Console.WriteLine(String.Format("{0}: {1}", responseDict.Keys.FirstOrDefault(), responseDict.Values.FirstOrDefault()));
+
+
+
+async Task<Dictionary<bool,string>> IsUKPostcodeValid(string ukPostcode) {
+
+    HttpClient httpClient = new HttpClient();
+
+    string uri = GET_BASE_URL + ukPostcode;
+
+    var response = await httpClient.GetAsync(uri);
+    if (response.IsSuccessStatusCode)
+    {
+        string dataResponse = await response.Content.ReadAsStringAsync();
+        return new Dictionary<bool, string> { [true] = dataResponse };
+    } else
+    {
+        return new Dictionary<bool, string> { [false] = "" };
+    }
+}
