692e08a624a0d662c22a1be1e0845ffd12aab83a
rgomezr <ramon@ramongomez.xyz>
2022-09-28 10:33:41 +0200

    Adding an additional manual thread apart from main thread to see how concurrency happens

--- concurrency_deadlock/Program.cs
+++ concurrency_deadlock/Program.cs
@@ -1,24 +1,33 @@
 ﻿const string GET_BASE_URL = "https://api.postcodes.io/postcodes/";
 
-Dictionary<bool, string> responseDict = await IsUKPostcodeValid("BT35 8HR");
+//Dictionary<bool, string> responseDict = await IsUKPostcodeValid("BT35 8HR");
 
-Console.WriteLine(String.Format("{0}: {1}", responseDict.Keys.FirstOrDefault(), responseDict.Values.FirstOrDefault()));
+//Console.WriteLine(String.Format("{0}: {1}", responseDict.Keys.FirstOrDefault(), responseDict.Values.FirstOrDefault()));
+
+Thread thread_1 = new(IsUKPostcodeValid);
+
+thread_1.Start("test"); // thread_1 starting task
+Thread.Sleep(10000); //main thread sleeping - This will give enough main time for thread_1 and threadpool to complete their corresponding tasks.
 
 
 
-async Task<Dictionary<bool,string>> IsUKPostcodeValid(string ukPostcode) {
 
-    HttpClient httpClient = new HttpClient();
+ static async void IsUKPostcodeValid(object? ukPostcode) {
+    Console.WriteLine("entering method");
+    HttpClient httpClient = new();
 
     string uri = GET_BASE_URL + ukPostcode;
-
-    var response = await httpClient.GetAsync(uri);
+    Console.WriteLine("before await");
+    var response = await httpClient.GetAsync(uri).ConfigureAwait(false);
+    Console.WriteLine("after await");
     if (response.IsSuccessStatusCode)
     {
-        string dataResponse = await response.Content.ReadAsStringAsync();
-        return new Dictionary<bool, string> { [true] = dataResponse };
+        string dataResponse = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        Console.WriteLine("valid");
+        //return new Dictionary<bool, string> { [true] = dataResponse };
     } else
     {
-        return new Dictionary<bool, string> { [false] = "" };
+        Console.WriteLine("invalid");
+        //return new Dictionary<bool, string> { [false] = "" };
     }
 }
