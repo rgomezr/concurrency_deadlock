1d505fcf1271ff6b275e4af55fe39bfaadb4dcec
rgomezr <ramon@ramongomez.xyz>
2022-10-12 10:06:45 +0200

    Added xUnit Testing

---
+++ PostcodeAPI.Tests/.cs
@@ -0,0 +1,10 @@
+namespace PostcodeAPI.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ PostcodeAPI.Tests/PostcodeAPI_testing.cs
@@ -0,0 +1,12 @@
+﻿namespace PostcodeAPI.Tests
+{
+    public class PostcodeAPI_testing
+    {
+        [Fact]
+        public void IsAPIReacheable_ReturnTrue()
+        {
+
+        }
+    }
+}
+
